9ee0bcafc483a90d60ee9a3fcd3306bc40bf57fd
sterczik <sterczewskik@gmail.com>
2018-11-12 22:41:41 +0100

    Move Todo methods to TodoController

--- src/api/controllers/TodoController.ts
+++ src/api/controllers/TodoController.ts
@@ -1,2 +1,32 @@
-// import { validate } from 'class-validator';
-// import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
+import * as HTTPStatus from 'http-status';
+import { getRepository } from 'typeorm';
+import { Todo } from '../entity/Todo';
+
+export async function getAll(req: Request, res: Response) {
+  const todos = await getRepository(Todo).find();
+  res.status(HTTPStatus.OK).json(todos);
+}
+
+export async function getOne(req: Request, res: Response) {
+  const todo = await getRepository(Todo).findOne({ where: { id: req.params.id } });
+  res.status(HTTPStatus.OK).json(todo);
+}
+
+export async function create(req: Request, res: Response) {
+  const todo = await getRepository(Todo).create(req.body);
+  await getRepository(Todo).save(todo);
+  res.status(HTTPStatus.OK).json(todo);
+}
+
+export async function edit(req: Request, res: Response) {
+  const todo = await getRepository(Todo).findOne({ where: { id: req.params.id } });
+  todo.name = req.body.name;
+  await getRepository(Todo).save(todo);
+  res.status(HTTPStatus.OK).json(todo);
+}
+
+export async function remove(req: Request, res: Response) {
+  await getRepository(Todo).delete(req.params.id);
+  res.status(HTTPStatus.OK).json(HTTPStatus.OK);
+}
--- src/api/routes/TodoRouter.ts
+++ src/api/routes/TodoRouter.ts
@@ -1,38 +1,13 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { Todo } from '../entity/Todo';
 import { authJwt } from '../services/auth';
-// import TodoController from '../controllers/TodoController';
+import * as TodoController from '../controllers/TodoController';
 
 const router = express.Router();
 
-router.get('/', authJwt, async (req: Request, res: Response) => {
-  const todos = await getRepository(Todo).find();
-  res.send(todos);
-});
-
-router.get('/:id', authJwt, async (req: Request, res: Response) => {
-  const todo = await getRepository(Todo).findOne({ where: { id: req.params.id } });
-  res.send(todo);
-});
-
-router.post('/', authJwt, async (req: Request, res: Response) => {
-  const todo = await getRepository(Todo).create(req.body);
-  await getRepository(Todo).save(todo);
-  res.send(todo);
-});
-
-router.put('/:id', authJwt, async (req: Request, res: Response) => {
-  const todo = await getRepository(Todo).findOne({ where: { id: req.params.id } });
-  todo.name = req.body.name;
-  await getRepository(Todo).save(todo);
-  res.sendStatus(200);
-});
-
-router.delete('/:id', authJwt, async (req: Request, res: Response) => {
-  await getRepository(Todo).delete(req.params.id);
-  res.sendStatus(200);
-});
+router.get('/', authJwt, TodoController.getAll);
+router.get('/:id', authJwt, TodoController.getOne);
+router.post('/', authJwt, TodoController.create);
+router.put('/:id', authJwt, TodoController.edit);
+router.delete('/:id', authJwt, TodoController.remove);
 
 export default router;
