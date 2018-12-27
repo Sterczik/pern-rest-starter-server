1741bd3c3eefb9e16bf1c4a33b8b8371c7a1974a
Sterczik <sterczewskik@gmail.com>
2018-12-27 22:16:50 +0100

    Heroku env

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import { createConnection } from "typeorm";
 import app from './config/express';
 import { env, port } from './config/variables';
 
+const APP_PORT = port || '3000';
+
 export const startServer = () => {
   createConnection()
   .then(() => {
-    app.listen(port, () => console.info(`Server started: Port ${port}, Env ${env} 🍺`));
+    app.listen(APP_PORT, () => console.info(`Server started: Port ${APP_PORT}, Env ${env} 🍺`));
   })
   .catch(err => console.log(err));
 }
