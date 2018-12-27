7958e2abad26c236a9bbbd22d98ab0e77f17f97e
Sterczik <sterczewskik@gmail.com>
2018-12-28 00:45:40 +0100

    Again change db envs

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   "type": "postgres",
-  "host": process.env.DATABASE_URL,
+  "host": process.env.DB_HOST,
   "port": process.env.DB_PORT,
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
